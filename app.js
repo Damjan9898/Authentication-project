1f69e12165ff227449840151c986aed426458518
unknown <damjanbelojevic98.db@gmail.com>
2023-02-20 12:44:12 +0100

    last commit

--- app.js
+++ app.js
@@ -3,19 +3,19 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose")
-// const encrypt = require("mongoose-encryption")
-// const md5 = require("md5");
-// const bcrypt = require('bcrypt');
-// //Koliko puta da se ponovo hashuje lozinka uz salt vrednost
-// const saltRounds = 10;
 const session = require('express-session')
 const passport = require('passport')
 const passportLocalMongoose = require('passport-local-mongoose')
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate')
+
+
+
 
 const app = express();
 
 app.use(session({
-    secret: 'Our little secret.',
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: false,
 }))
@@ -24,7 +24,6 @@ app.use(passport.initialize());
 
 app.use(passport.session());
 
-console.log(process.env.SECRET)
 
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 
@@ -36,29 +35,78 @@ app.set('view engine', 'ejs')
 
 const userSchema = new mongoose.Schema({
     username : String,
-    password: String
+    password: String,
+    googleId : String,
+    secret: String
 })
+
+// const secretSchema = new mongoose.Schema({
+//     username : String,
+//     password: String,
+//     googleId : String
+// })
 
 
 userSchema.plugin(passportLocalMongoose);
-
-//Bitno je da bude pre mongoose modela
-// process.env.SECRET je varijabla iz .env fajla koji je nevidljiv
-// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
+userSchema.plugin(findOrCreate);
 
 const User = mongoose.model("User", userSchema)
 
-
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, cb) {
+    process.nextTick(function() {
+      cb(null, { id: user.id, username: user.username, name: user.name });
+    });
+  });
+  
+passport.deserializeUser(function(user, cb) {
+    process.nextTick(function() {
+      return cb(null, user);
+    });
+});
+
+//Bitno da bude ovde
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    //Ovo se dodaje zbog odedjenog issue
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+    profileFields: ['id', 'emails', 'name']
+  },
+  function(accessToken, refreshToken, profile, cb) {
+
+    //Ovako nor od gugla mogu da izvucem ime i prezime i upisem u bazu
+    //Profile : prikaz celog profila
+    // profile.name.family_name - Prikaz prezimena
+    // profile.name.givenName - Prikaz imena
+    // profile.emails[0].value - Prikaz email-a
+    
+    console.log(profile)
+
+    User.findOrCreate({ googleId: profile.id, username: profile.emails[0].value }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 
 
 app.get("/", (req, res)=>{
     res.render("home")
 })
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+  
+app.get('/auth/google/secrets', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect to secrets.
+    res.redirect('/secrets');
+});
 
 app.get("/login", (req, res)=>{
     res.render("login")
@@ -70,11 +118,12 @@ app.get("/register", (req, res)=>{
 
 app.get("/secrets", (req, res)=>{
     
-        if(req.isAuthenticated()){
-            res.render("secrets")
-        }else{
-            res.redirect("/login")
+    //Not equal to null
+    User.find({secret: {$ne:null}}, (err, users)=>{
+        if(!err){
+            res.render("secrets", {usersWithSecrets: users})
         }
+    })
 })
 
 app.get("/logout", (req, res)=>{
@@ -86,7 +135,37 @@ app.get("/logout", (req, res)=>{
     });
     
 
-})
+});
+
+app.get("/submit", (req, res)=>{
+
+    if(req.isAuthenticated()){
+        res.render("submit")
+    }else{
+        res.redirect("/login")
+    }
+});
+
+app.post("/submit", (req, res)=>{
+
+   let newSecret = req.body.secret;
+
+   User.findById(req.user.id, (err, user)=>{
+        if(!err){
+            user.secret = newSecret;
+            user.save((err)=>{
+                if(!err){
+                    res.redirect("/secrets")
+                }
+            })
+        }
+   })
+
+   console.log(req.user.id)
+
+});
+
+
 
 
 
@@ -112,41 +191,6 @@ app.post("/register", (req, res)=>{
     
       });
 
-    //Bcrypt
-    // bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-    //     if(!err){
-    //         const user = new User({
-    //             email : req.body.email,
-    //             password: hash
-    //         })
-    
-    //         user.save((err)=>{
-    //             if(!err){
-    //                 res.render("secrets")
-    //             }else{
-    //                 console.log(err)
-    //             }
-    //         });
-    //     }
-        
-    // });
-
-
-    
-    //MD5 lozinka
-    // const user = new User({
-    //     email : req.body.email,
-    //     password: md5(req.body.password)
-    // })
-
-    // user.save((err)=>{
-    //     if(!err){
-    //         res.render("secrets")
-    //     }else{
-    //         console.log(err)
-    //     }
-    // });
-
 })
 
 app.post("/login", (req, res)=>{
@@ -167,26 +211,5 @@ app.post("/login", (req, res)=>{
             res.redirect("/")
         }
     })
-
-    //Bcrypt
-    // User.findOne({email:req.body.email}, (err, user)=>{
-    //     if(!err){
-    //         //md5
-    //         // if(user.password === md5(req.body.password)){
-    //         //     res.render("secrets")
-    //         // }else{
-    //         //   console.log("Good email, wrong password")  
-    //         // }
-
-    //         bcrypt.compare(req.body.password, user.password, function(err, result) {
-    //             if(result){
-    //                 res.render("secrets")
-    //             }
-    //         });
-
-    //     }else{
-    //         console.log(err)
-    //     }
-    // })
 
 })
